43619c37d279a281b649977fc01c565c1046444b
felixOlesen <felixolesen@gmail.com>
2026-08-08 23:30:02 +0100

    plan/added in planning for the triage system

--- spec_tagger/spec_review/result_triage.py
+++ spec_tagger/spec_review/result_triage.py
@@ -1,3 +1,13 @@
 class ResultTriage:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, context_data: dict) -> None:
+        self.context_data = context_data
+
+    # Extract Invalid Tags
+    # Extract Test Failures
+    # Un-covered tests and test files
+    # Change Analysis:
+    # Compare Changed files to their respective passing tests.
+    #   - Figure this out from line numbers
+    # Has a spec file changed?
+    # Has a test file changed?
+    # Has a code file changed and do the tests cover it?
