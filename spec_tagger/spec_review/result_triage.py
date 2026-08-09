5575ac341008b4221c86a584dbf8a61142c265a8
felixOlesen <felixolesen@gmail.com>
2026-08-09 22:39:40 +0100

    feature/implemented triage function for missed test cases and test files

--- spec_tagger/spec_review/result_triage.py
+++ spec_tagger/spec_review/result_triage.py
@@ -5,10 +5,12 @@ from spec_tagger.spec_review.solution import Solution
 
 
 class ProblemType(Enum):
-    INVALID_TAG = 1
-    TEST_FAILURE = 2
-    MISSED_TEST_OR_FILE = 3
-    PASSED_BUT_CHANGED = 4
+    INVALID_TAG = ""
+    TEST_FAILURE = ""
+    TEST_ERROR = ""
+    MISSED_FILE = ""
+    MISSED_TEST = ""
+    PASSED_BUT_CHANGED = ""
 
 
 class SolutionMessages(Enum):
@@ -19,6 +21,11 @@ class SolutionMessages(Enum):
     NO_FUNCTION_FOR_TEST_TAG = "Double check that the test tag has been placed in the right location, either remove it, relocate it, or write a new test to fit the test tag and spec tag items."
     NO_SPEC_TAG_FOR_TEST_TAG = "Enure that the tests written have a respective spec tag and portion of the spec dedicated to the test."
     TAG_REVISION_MISMATCH = "Take a look at the older/lower revision numbers to see if anything in their content needs to be updated according to how the tag content with the latest revision number looks. If not, just bump the revision numbe rup to the latest, or adjust the content if so."
+    UNCOVERED_FILE = "Read through the file contents to understand where in the spec they adhere to in terms of functionality, then either apply pre-existing spec tags to the tests in the file one-by-one, or write new entries for the specification to describe the behaviour that is being tested in the test file."
+    UNCOVERED_TEST = "Read through the test contents to see what part of the spec it might belong to, either apply a pre-existing tag to the test or write a new entry into the spec with a new tag which you can then apply to the test."
+    FAILED_TEST = "Take a look at the test output of the failing test to make sure that the behaviour it tests is correct in accordance to the spec item, also take a look at recently changed portions of the implementation code to figure out if anything might be incorrect, or not implemented yet."
+    ERROR_TEST = "The test has returned an error instead of being able to finish and provide a result, make sure to take a look at the error output and the test function block to be sure there are no errors in your test code, then look at the implementation code to see if any errors are apparent there as well."
+    PASSED_BUT_CHANGED = "Everything in terms of test-results appears to be running correctly, although it seems that teh files that are connected to the spec, test, or implementation code have changed substantially, make sure to check these diffs in the code and evaluate if behaviourally they are all consistent with eachother."
 
 
 class ResultTriage:
@@ -31,7 +38,14 @@ class ResultTriage:
             "tag_coverage"
         ]
         self.test_coverage = context_data["report"]["coverage_data"]["test_coverage"]
-        self.test_failures = context_data["report"]["test_results"]
+        self.test_failures = []
+        self.test_passes = []
+
+        for tag, result in context_data.items():
+            if result["outcome"] == "passed":
+                self.test_passes.append({"tag": tag, "result": result})
+            elif result["outcome"] == "failed":
+                self.test_failures.append({"tag": tag, "result": result})
 
         self._print_keys(self.git_context, "Git Context", True)
         self._print_keys(self.invalid_tags, "Invalid Tags")
@@ -39,7 +53,6 @@ class ResultTriage:
             self.uncovered_tests_and_files, "Un-Covered Tests and Files", True
         )
         self._print_keys(self.test_coverage, "Test Coverage")
-        self._print_keys(self.test_failures, "Test Failures")
 
         self.solutions = []
 
@@ -67,7 +80,7 @@ class ResultTriage:
             for invalid in self.invalid_tags:
                 solution = Solution(
                     related_tag=invalid["full_tag"],
-                    tag_location=f"{invalid['filename']}:{invalid['line']}",
+                    location=f"{invalid['filename']}:{invalid['line']}",
                     item=invalid["content"],
                     git_commit_messages=git_context.commits_for_file(
                         invalid["filename"],
@@ -109,22 +122,80 @@ class ResultTriage:
                             solution.solution_statement.append(
                                 SolutionMessages.TAG_REVISION_MISMATCH.value
                             )
+                    self.solutions.append(solution)
         else:
             print("No invalid tags found during triage")
 
     def _triage_missed_tests_and_files(self):
-        pass
+        if self.uncovered_tests_and_files["files"]:
+            for file in self.uncovered_tests_and_files["files"]:
+                solution = Solution(
+                    related_tag=None,
+                    location=file,
+                    item=None,
+                    git_commit_messages=git_context.commits_for_file(
+                        file, self.git_context["base"], self.git_context["head"]
+                    ),
+                    git_diff=git_context.diff_for_file(
+                        file, self.git_context["base"], self.git_context["head"]
+                    ),
+                    test_coverage=None,
+                    problem_type=ProblemType.MISSED_FILE,
+                    problem_statement=[ProblemType.MISSED_FILE.value],
+                    solution_statement=[SolutionMessages.UNCOVERED_FILE.value],
+                    ai_enabled=self.ai_enabled,
+                    ai_usage_recommended=True,
+                )
+                self.solutions.append(solution)
+        if self.uncovered_tests_and_files["tests"]:
+            for test in self.uncovered_tests_and_files["tests"]:
+                solution = Solution(
+                    related_tag=None,
+                    location=f"{test['file']}:{test['line']}",
+                    item=None,
+                    git_commit_messages=git_context.commits_for_file(
+                        test["file"], self.git_context["base"], self.git_context["head"]
+                    ),
+                    git_diff=git_context.diff_for_file(
+                        test["file"], self.git_context["base"], self.git_context["head"]
+                    ),
+                    test_coverage=None,
+                    problem_type=ProblemType.MISSED_TEST,
+                    problem_statement=[ProblemType.MISSED_TEST.value],
+                    solution_statement=[SolutionMessages.UNCOVERED_TEST.value],
+                    ai_enabled=self.ai_enabled,
+                    ai_usage_recommended=True,
+                )
+                self.solutions.append(solution)
 
     def _triage_failed_tests(self):
+        # if self.test_failures:
+        #     for test_fail in self.test_failures:
+        #         if test_fail["result"]["stderr"]:
+        #             solution = Solution(
+        #                 related_tag=None,
+        #                 location=f"{test['file']}:{test['line']}",
+        #                 item=None,
+        #                 git_commit_messages=git_context.commits_for_file(
+        #                     test["file"], self.git_context["base"], self.git_context["head"]
+        #                 ),
+        #                 git_diff=git_context.diff_for_file(
+        #                     test["file"], self.git_context["base"], self.git_context["head"]
+        #                 ),
+        #                 test_coverage=None,
+        #                 problem_type=ProblemType.TEST_ERROR,
+        #                 problem_statement=[ProblemType.TEST_ERROR.value],
+        #                 solution_statement=[SolutionMessages.ERROR_TEST.value],
+        #                 ai_enabled=self.ai_enabled,
+        #                 ai_usage_recommended=True,
+        #             )
+        #             self.solutions.append(solution)
+        #         else:
         pass
 
     def _triage_passed_but_changed_tests(self):
-        pass
-
-    def _register_non_ai_entry(self):
-        pass
-
-    def _register_ai_recommended_entry(self):
+        for test_pass in self.test_passes:
+            continue
         pass
 
     # Invalid Tag Handling
--- spec_tagger/spec_review/solution.py
+++ spec_tagger/spec_review/solution.py
@@ -4,21 +4,21 @@ from spec_tagger.spec_review.result_triage import ProblemType
 class Solution:
     def __init__(
         self,
-        related_tag: str,
-        tag_location: str,
-        item: str,
-        git_commit_messages: list[str],
-        git_diff: str,
-        test_coverage,
-        problem_type: ProblemType,
+        related_tag: str | None,
+        location: str | None,
+        item: str | None,
+        git_commit_messages: list[str] | None,
+        git_diff: str | None,
+        test_coverage: dict | None,
+        problem_type: ProblemType | None,
         solution_statement: list[str],
         problem_statement: list[str],
-        tag_coverage=None,
+        tag_coverage: dict | None = None,
         ai_enabled: bool = False,
         ai_usage_recommended: bool = False,
     ):
         self.related_tag = related_tag
-        self.tag_location = tag_location
+        self.location = location
         self.problem_type = problem_type
         self.item = item
         self.solution_statement = solution_statement
